afcd5d190160d67e57d4d3b52871559fee96dbe6
parkhyeonji <iamphj3@naver.com>
2023-07-10 10:31:41 -0700

    design : 이름 입력 input창 스타일링

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,12 @@ import styled from 'styled-components';
 const Login = () => {
     return (
         <StLoginWrapper >
-            로그인 컴포넌트
+            <h1> 나는 슬플 때 노래를 들어..♬ </h1>
+            <p>[SPP 2023] Team zㅣ존얼짱s2</p>
+            <StNameInput>
+                <label for="user_name">이름 : </label>
+                <input type="text" name="user_name" value="name"></input>
+            </StNameInput>
         </StLoginWrapper>
     );
 };
@@ -12,12 +17,55 @@ const Login = () => {
 export default Login;
 
 const StLoginWrapper = styled.div`
-    background: linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url("./BGminiroom.gif");
-    background-size: cover;
-    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
     width: 100%;
     height: 100vh;
 
-    font-family: 'NeoDunggeunmo';
-    font-size: 1.6rem;
+    background: linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url("./BGminiroom.gif");
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    & > h1 {
+        color: white;
+        font-family: 'NeoDunggeunmo';
+        font-size: 4rem;    
+        font-weight: 700;
+    }
+
+    & > p {
+        padding-top: 2rem;
+        
+        color: white;
+        font-family: 'NeoDunggeunmo';
+        font-size: 1.8rem;    
+        font-weight: 300;
+    }
 `
+
+const StNameInput = styled.div`
+    padding-top: 4rem;
+
+    color: white;
+    font-family: 'NeoDunggeunmo';
+    font-size: 3.6rem;    
+    font-weight: 500;
+
+    & > input {
+        width: 20rem;
+        height: 3rem;
+        padding-left: 1rem;
+
+        border: 0.1rem solid white;
+        border-radius: 1rem;
+        background: transparent;
+
+        color: white;
+        font-family: 'NeoDunggeunmo';
+        font-size: 2rem;    
+        font-weight: 100;
+    }    
+`;
