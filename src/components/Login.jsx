f54942c7e1fe6a0c15ccb50619c720ac71572933
parkhyeonji <iamphj3@naver.com>
2023-07-10 10:07:51 -0700

    chore : 로그인 컴포넌트 배경 이미지 gif 추가

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-function Login() {
-  return (
-    <div>
-      로그인 컴포넌트
-    </div>
-  )
-}
+const Login = () => {
+    return (
+        <StLoginWrapper >
+            로그인 컴포넌트
+        </StLoginWrapper>
+    );
+};
 
-export default Login
+export default Login;
+
+const StLoginWrapper = styled.div`
+    background: linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url("./BGminiroom.gif");
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100vh;
+`
