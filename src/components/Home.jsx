035b5d9272dc6e335a097988f484faaed803fc3a
parkhyeonji <iamphj3@naver.com>
2023-07-10 10:44:44 -0700

    design : Home 페이지 스타일링

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import styled from 'styled-components';
 
 const Home = () => {
+    const [userName, setUsername] = useState("");
+
     return (
         <StHomeWrapper >
-            <h1> 나는 슬플 때 노래를 들어..♬ </h1>
-            <p>[SPP 2023] Team zㅣ존얼짱s2</p>
+            <h1>I listen to songs when I'm sad..♬ </h1>
+            <p>[SPP 2023] Team Zizon-Ulzzang</p>
             <StNameInput>
-                <label for="user_name">이름 : </label>
-                <input type="text" name="user_name" value="name"></input>
+                <label for="userName">name :
+                    <input type="text" name="userName" placeholder='Enter your name.'></input>
+                </label>
+                <button type="button"> Today is...</button>
             </StNameInput>
         </StHomeWrapper>
     );
@@ -47,6 +52,11 @@ const StHomeWrapper = styled.div`
 `
 
 const StNameInput = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
     padding-top: 4rem;
 
     color: white;
@@ -54,10 +64,10 @@ const StNameInput = styled.div`
     font-size: 3.6rem;    
     font-weight: 500;
 
-    & > input {
+    & > label > input {
+        margin-left: 1rem;
         width: 20rem;
         height: 3rem;
-        padding-left: 1rem;
 
         border: 0.1rem solid white;
         border-radius: 1rem;
@@ -67,5 +77,27 @@ const StNameInput = styled.div`
         font-family: 'NeoDunggeunmo';
         font-size: 2rem;    
         font-weight: 100;
+        
+        text-align: center;
+
     }    
+    ::placeholder {
+        color: white;
+        font-family: 'NeoDunggeunmo';
+        font-size: 2rem;    
+        font-weight: 100;
+    }
+
+    & > button {
+        width: 17rem;
+        margin-top: 4.5rem;
+
+        border: 0.3rem solid white;
+        color: white;
+        background-color: rgba(255,255,255, 0.164);
+        font-family: 'NeoDunggeunmo';
+        font-size: 1.8rem;
+        font-weight: 500;
+        line-height: 3.5rem;
+    }
 `;
