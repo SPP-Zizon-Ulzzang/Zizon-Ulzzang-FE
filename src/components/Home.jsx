7cf8cf507910f944b87a8932957677ac3329f090
parkhyeonji <iamphj3@naver.com>
2023-07-11 14:57:57 -0700

    chore : add background image to result page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,7 @@ const StHomeWrapper = styled.div`
     width: 100%;
     height: 100vh;
 
-    background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9)), url("./BG.png") center;
+    background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9)), url("./BG_home.png") center;
     background-size: cover;
     background-repeat: no-repeat;
 
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,10 +3,19 @@ import styled from 'styled-components';
 
 const Result = () => {
     return (
-        <div>
+        <StResultWrapper>
             결과 컴포넌트
-        </div>
+        </StResultWrapper>
     );
 };
 
 export default Result;
+
+const StResultWrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    background: url("./BG.png") center;
+    background-size: cover;
+    background-repeat: no-repeat;
+`;
