8ae8fe1f210f52bc7956cdc7ac72890d40c8940f
parkhyeonji <iamphj3@naver.com>
2023-07-12 14:43:13 -0700

    feat : get color, description from mbti constant

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,15 +4,19 @@ import { MBTI_RESULT } from '../constants/mbti';
 import styled from 'styled-components';
 
 const Result = () => {
-    const [mbti, setMbti] = useState("ISTP");
+    const [mbti, setMbti] = useState("ENFP");
+    const mbtiInfo = MBTI_RESULT.find(item => item.MBTI === mbti);
     
     // TODO
     // 1. get MBTI result from server
 
     return (
         <StResultWrapper>
-            <StResult>
+            <StResult mbtiColor={mbtiInfo.COLOR}>
                 <h1>{mbti}</h1>
+                <StDescription>
+                    <p>{mbtiInfo.DESCRIPTION}</p>
+                </StDescription>
             </StResult>
         </StResultWrapper>
     );
@@ -22,6 +26,7 @@ export default Result;
 
 const StResultWrapper = styled.section`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     overflow-y: scroll;
@@ -34,7 +39,7 @@ const StResultWrapper = styled.section`
     background-repeat: repeat-y;
 
     & > img {
-        width: 45%;
+        width: 60rem;
 
         object-fit: contain;
     }
@@ -42,6 +47,10 @@ const StResultWrapper = styled.section`
 
 
 const StResult = styled.article`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
     width: 60rem;
     height: 60rem;
 
@@ -49,13 +58,31 @@ const StResult = styled.article`
     background-size: cover;
 
     & > h1 {
-        margin: 10rem 0 0 5rem;
-
-        color: white;
+        margin: 10rem 0 0 -30rem;
+        
+        color : ${props => props.mbtiColor};
         font-family: 'NeoDunggeunmo';
         font-size: 10rem;
         font-weight: 800;
 
         text-shadow: -0.3rem 0 black, 0 0.3rem black, 0.3rem 0 black, 0 -0.3rem black;
+    }
+`;
+
+const StDescription = styled.section`
+    overflow-y: scroll;
+
+    width: 50rem;
+    height: 25rem;
+    
+    margin-top: 5rem;
+
+    & > p {
+        padding: 0 2rem;
+        
+        font-family: 'NeoDunggeunmo';
+        font-size: 1.6rem;
+        font-weight: 300;
+        line-height: 2rem;
     }
 `;
