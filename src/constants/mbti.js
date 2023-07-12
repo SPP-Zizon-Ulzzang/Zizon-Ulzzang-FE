6ea65762daae7154570fdb64204d3b6f0e175e9d
parkhyeonji <iamphj3@naver.com>
2023-07-12 13:55:43 -0700

    chore : add mbti.js constant file

---
+++ src/constants/mbti.js
@@ -0,0 +1,82 @@
+const MBTI_RESULT = [
+    {
+        MBTI: "ENFJ",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ENTJ",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ENFP",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ENTP",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ESFP",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ESFJ",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ESTP",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ESTJ",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "INFP",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "INFJ",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "INTP",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ISTP",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ISFP",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ISFJ",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "ISTJ",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+    {
+        MBTI: "INTJ",
+        DESCRIPTION: "",
+        COLOR: "",
+    },
+]
