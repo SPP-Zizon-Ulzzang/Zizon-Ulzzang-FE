068a71593d32360c760c43dc8752c2b9400e41d3
parkhyeonji <iamphj3@naver.com>
2023-07-17 16:58:45 -0700

    fix : change request body -> query

--- src/lib/api.js
+++ src/lib/api.js
@@ -10,9 +10,7 @@ export const client = axios.create({
 export const getMBTI = async (snsUrl) => {
   try {
     console.log("snsUrl: ", snsUrl);
-    const data = await client.get('/sns/instagram', {
-      snsUrl: snsUrl,
-    });
+    const data = await client.get(`/sns/instagram?snsUrl=${snsUrl}`);
     console.log("data: ", data);
     return data;
   } catch (err) {
