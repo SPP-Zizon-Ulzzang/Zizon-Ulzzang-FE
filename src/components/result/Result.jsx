be4306be66d67a2dcac3797f6824ed747d7c140e
parkhyeonji <iamphj3@naver.com>
2023-07-18 16:09:22 -0700

    design : set content box position

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -43,23 +43,28 @@ const Result = () => {
             <StResult>
                 <StMbtiResult mbtiColor={mbtiInfo.COLOR}>
                     <h1>{mbti}</h1>
-                    <p>어쩌고저쩌고 설명 적을꺼임</p>
-                    <p>어쩌고저쩌고 설명 적을꺼임</p>
-                    <p>어쩌고저쩌고 설명 적을꺼임</p>
-                    <p>어쩌고저쩌고 설명 적을꺼임</p>
-                </StMbtiResult>    
-                <StDescription>
-                    <h2>Description</h2>    
-                    <p>{mbtiInfo.DESCRIPTION}</p>
-                </StDescription>
-                {/* <StCelebrity>
-                    <h2>Celebrity</h2>    
-                    <p>{mbtiInfo.DESCRIPTION}</p>
-                </StCelebrity> */}
-                <StBtnWrapper>
-                    <Button btnName="Do It Again" onClick={() => {navigate('/')}}/>
-                    <Button btnName="Copy Link" onClick={() => {navigate('/')}}/>
-                </StBtnWrapper>
+                    <p>어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임 어쩌고저쩌고 설명 적을꺼임</p>
+                </StMbtiResult>
+
+                <StMbtiContents>
+                    <StDescription>
+                        <h2>Description</h2>    
+                        <p>{mbtiInfo.DESCRIPTION}</p>
+                    </StDescription>
+                    <StBtnWrapper>
+                        <Button btnName="Do It Again" onClick={() => {navigate('/')}}/>
+                        <Button btnName="Copy Link" onClick={() => {navigate('/')}}/>
+                    </StBtnWrapper>
+                    <StCelebrity>
+                        <h2>Celebrity</h2>    
+                        <p>이런 연예인이랑 같은 mbti입니당</p>
+                    </StCelebrity>
+                    <StChemistry>
+                        <h2>Chemistry</h2>    
+                        <p>mbti 궁합</p>
+                    </StChemistry>
+                </StMbtiContents>
+
             </StResult>
         </StResultWrapper >
         :
@@ -69,7 +74,7 @@ const Result = () => {
 
 export default Result;
 
-const StResultWrapper = styled.section`
+const StResultWrapper = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -78,6 +83,7 @@ const StResultWrapper = styled.section`
     
     width: 100%;
     height: 170vh;
+    padding-top: 10rem;
     
     background: url("./BG.png") center;
     background-size: contain;
@@ -85,7 +91,7 @@ const StResultWrapper = styled.section`
     font-size: 10rem;
 `;
 
-const StResult = styled.section`
+const StResult = styled.div`
     display: flex;
     flex-direction: column;
     
@@ -99,10 +105,13 @@ const StResult = styled.section`
 `;
 
 const StMbtiResult = styled.header`
+    width: 73rem;
+    margin: 14rem 0 0 8rem;
+    
     font-family: 'NeoDunggeunmo';
 
     & > h1 {
-        margin: 14rem 0 0 10rem;
+        margin-bottom: 1rem;
         
         color : ${props => props.mbtiColor};
         font-size: 12rem;
@@ -110,15 +119,59 @@ const StMbtiResult = styled.header`
 
         text-shadow: -0.3rem 0 black, 0 0.3rem black, 0.3rem 0 black, 0 -0.3rem black;
     }
-    & > p {
-        font-size: 2rem;
+    & > p {  
+        overflow-y : hidden;
+        height: 10rem;
+        
+        font-size: 1.8rem;
     }
 `;
 
-const StDescription = styled.article`
-    width: 40rem;
-    margin: 9rem 0 0 13rem;
+const StMbtiContents = styled.section`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
     
+    width: 100%;
+    height: 90rem;
+`;
+
+const StDescription = styled.article`
+    width: 42rem;
+    margin: 9.5rem 0 0 11rem;
+    
+    font-family: 'NeoDunggeunmo';
+
+    & > h2 {
+        margin-bottom: 1.5rem;
+
+        font-size: 3.5rem;
+        font-weight: 800;
+    }
+    & > p {
+        overflow-y: scroll;
+        
+        height: 44rem;
+        
+        font-size: 2rem;
+        font-weight: 300;
+        line-height: 2.6rem;
+    }
+`;
+
+const StBtnWrapper = styled.section`
+    display: flex;
+    gap : 2rem;
+    
+    margin: 13rem 0 0 10rem;
+`
+
+const StCelebrity = styled.article`
+    width: 30rem;
+    height: 35rem;
+    margin-top: 7rem;
+
+    background-color: yellow;
     font-family: 'NeoDunggeunmo';
 
     & > h2 {
@@ -130,20 +183,31 @@ const StDescription = styled.article`
     & > p {
         overflow-y: scroll;
         
-        height: 42rem;
-        
-        font-size: 2rem;
+        font-size: 1.8rem;
         font-weight: 300;
         line-height: 2.6rem;
     }
 `;
 
-const StCelebrity = styled.article`
-  width: 35rem;
+const StChemistry = styled.article`
+    width: 30rem;
+    height: 32rem;
+    margin: 10rem 0 0 1rem;
+
+    background-color: lightblue;
+    font-family: 'NeoDunggeunmo';
+
+    & > h2 {
+        margin-bottom: 1rem;
+
+        font-size: 3.5rem;
+        font-weight: 800;
+    }
+    & > p {
+        overflow-y: scroll;
+        
+        font-size: 1.8rem;
+        font-weight: 300;
+        line-height: 2.6rem;
+    }
 `;
-const StBtnWrapper = styled.section`
-    display: flex;
-    gap : 2rem;
-    
-    margin: 12rem 0 0 11rem;
-`
