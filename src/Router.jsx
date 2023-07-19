c2813c580a29755bd4f632c0f28732e4e758c547
parkhyeonji <iamphj3@naver.com>
2023-07-19 15:15:22 -0700

    chore : remove login, auth

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Auth from './components/Auth/Auth';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
 import React from 'react'
 import ResultPage from './pages/ResultPage';
 
@@ -12,8 +10,6 @@ function Router() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/login/auth" element={<Auth />} />
                 <Route path="/result" element={<ResultPage />} />
                 <Route path="*" element={<ErrorPage />} />
             </Routes>
