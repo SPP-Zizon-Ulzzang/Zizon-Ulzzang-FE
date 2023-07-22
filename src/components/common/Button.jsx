bb2913fc453e403f756902c635c0e79e5c3f2772
parkhyeonji <iamphj3@naver.com>
2023-07-22 11:25:11 -0700

    fix : fix button mobile UI

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -28,28 +28,35 @@ const StBtn = styled.button`
 
     &::before {
         content: '';
+    }
 
-        position: absolute;
-        top: 0;
-        left: 0;
-
-        width: 100%;
-        height: 100%;
-
-        background: #5A96E3;
-        border-radius: inherit;
-        box-shadow: 0 0 0 0.2rem #0A6EBD;
-
-        transform: translate3d(0, 0.75rem, -1rem);
-        transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
-        box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
-    } 
-    @media all and (min-width: 1023px) {
+    @media all and (min-width: 1023px) { // 데스크탑
         width: 18rem;
         height: 8rem;
         
         font-size: 2.4rem;
 
+        transform-style: preserve-3d;
+        transition: transform 150ms cubic-bezier(0, 0, 0.58, 1);
+        
+        &::before {
+            content: '';
+
+            position: absolute;
+            top: 0rem;
+            left: 0ch;
+
+            width: 100%;
+            height: 100%;
+
+            background: #5A96E3;
+            border-radius: inherit;
+            box-shadow: 0 0 0 0.2rem #0A6EBD;
+
+            transform: translate3d(0, 0.75rem, -1rem);
+            transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
+                        box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
+        } 
         &:hover {
             background: #5A96E3;
             color: white;
