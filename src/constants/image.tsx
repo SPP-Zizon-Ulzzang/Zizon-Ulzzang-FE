8f6fc477e098bc2efa2c74f50ee6ad88baab40c9
parkhyeonji <iamphj3@naver.com>
2023-09-04 17:40:43 +0900

    chore : chemistry_image RESULT_IMG에 추가

--- src/constants/image.tsx
+++ src/constants/image.tsx
@@ -1,4 +1,22 @@
 import {
+  IcENFJChemistry,
+  IcENFPChemistry,
+  IcENTJChemistry,
+  IcENTPChemistry,
+  IcESFJChemistry,
+  IcESFPChemistry,
+  IcESTJChemistry,
+  IcESTPChemistry,
+  IcINFJChemistry,
+  IcINFPChemistry,
+  IcINTJChemistry,
+  IcINTPChemistry,
+  IcISFJChemistry,
+  IcISFPChemistry,
+  IcISTJChemistry,
+  IcISTPChemistry,
+} from '../assets/icons/chemistry';
+import {
   IcENFJMain,
   IcENFPMain,
   IcENTJMain,
@@ -39,6 +57,7 @@ interface ResultImgEntry {
   MBTI: string;
   main: React.ReactNode;
   rank: React.ReactNode;
+  chemistry: React.ReactNode;
 }
 
 export const RESULT_IMG: ResultImgEntry[] = [
@@ -46,80 +65,96 @@ export const RESULT_IMG: ResultImgEntry[] = [
     MBTI: 'ENFJ',
     main: <IcENFJMain />,
     rank: <IcENFJRank />,
+    chemistry: <IcENFJChemistry />,
   },
   {
     MBTI: 'ENFP',
     main: <IcENFPMain />,
     rank: <IcENFPRank />,
+    chemistry: <IcENFJChemistry />,
   },
   {
     MBTI: 'ENTJ',
     main: <IcENTJMain />,
     rank: <IcENTJRank />,
+    chemistry: <IcENTJChemistry />,
   },
   {
     MBTI: 'ENTP',
     main: <IcENTPMain />,
     rank: <IcENTPRank />,
+    chemistry: <IcENTPChemistry />,
   },
   {
     MBTI: 'ESFJ',
     main: <IcESFJMain />,
     rank: <IcESFJRank />,
+    chemistry: <IcESFJChemistry />,
   },
   {
     MBTI: 'ESFP',
     main: <IcESFPMain />,
     rank: <IcESFPRank />,
+    chemistry: <IcESFPChemistry />,
   },
   {
     MBTI: 'ESTJ',
     main: <IcESTJMain />,
     rank: <IcESTJRank />,
+    chemistry: <IcESTJChemistry />,
   },
   {
     MBTI: 'ESTP',
     main: <IcESTPMain />,
     rank: <IcESTPRank />,
+    chemistry: <IcESTPChemistry />,
   },
   {
     MBTI: 'INFJ',
     main: <IcINFJMain />,
     rank: <IcINFJRank />,
+    chemistry: <IcINFJChemistry />,
   },
   {
     MBTI: 'INFP',
     main: <IcINFPMain />,
     rank: <IcINFPRank />,
+    chemistry: <IcINFPChemistry />,
   },
   {
     MBTI: 'INTJ',
     main: <IcINTJMain />,
     rank: <IcINTJRank />,
+    chemistry: <IcINTJChemistry />,
   },
   {
     MBTI: 'INTP',
     main: <IcINTPMain />,
     rank: <IcINTPRank />,
+    chemistry: <IcINTPChemistry />,
   },
   {
     MBTI: 'ISFJ',
     main: <IcISFJMain />,
     rank: <IcISFJRank />,
+    chemistry: <IcISFJChemistry />,
   },
   {
     MBTI: 'ISFP',
     main: <IcISFPMain />,
     rank: <IcISFPRank />,
+    chemistry: <IcISFPChemistry />,
   },
   {
     MBTI: 'ISTJ',
     main: <IcISTJMain />,
     rank: <IcISTJRank />,
+    chemistry: <IcISTJChemistry />,
   },
   {
     MBTI: 'ISTP',
     main: <IcISTPMain />,
     rank: <IcISTPRank />,
+    chemistry: <IcISTPChemistry />,
   },
 ];
--- src/utils/mapMBTIToImage.ts
+++ src/utils/mapMBTIToImage.ts
@@ -4,9 +4,11 @@ export const mapMBTIToImage = (mbti: string) => {
   const matchingMBTI = RESULT_IMG.find((item) => item.MBTI === mbti);
   const main_image = matchingMBTI?.main;
   const rank_image = matchingMBTI?.rank;
+  const chemistry_image = matchingMBTI?.chemistry;
 
   return {
     main_image,
     rank_image,
+    chemistry_image,
   };
 };
